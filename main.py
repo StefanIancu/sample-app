106df948a2c2aec6aa0c287fe1cf2f39901fb65f
Stefan Iancu <stefan.iancu@safeinit.com>
2026-03-22 11:29:13 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+app = FastAPI(title="Sample App")
+
+
+@app.get("/")
+def root():
+    return {"message": "Hello from Sample App!"}
+
+
+@app.get("/health")
+def health():
+    return {"status": "ok"}
