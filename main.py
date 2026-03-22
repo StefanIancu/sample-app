71d1aa5f1279f5eee59537de48b61bdc57b70062
Stefan Iancu <stefan.iancu@safeinit.com>
2026-03-22 22:25:42 +0200

    chore: test real time logs v2.3

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = FastAPI(title="Sample App")
 
 @app.get("/")
 def root():
-    return {"message": "Hello from Sample App - Deployment v2.2!"}
+    return {"message": "Hello from Sample App - Deployment v2.3!"}
 
 
 @app.get("/health")
